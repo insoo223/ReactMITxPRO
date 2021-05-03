05bd7b2ce34c726be5c8050618bb890bdcf8939b
Insoo Kim <insoo@timmanage.com>
2021-05-03 14:32:44 +0900

    Add To-Do List on TIM Server
    implemented on "tsdodo" folder using create-react-app toolchain. Reference document: MIT xPRO-FEDR-4주차, 리액트 리스트, React FrontEnd-10533154  https://timmanagecom.sharepoint.com/:w:/r/sites/Merry/Shared%20Documents/MIT%20xPRO-FEDR-4%EC%A3%BC%EC%B0%A8,%20%EB%A6%AC%EC%95%A1%ED%8A%B8%20%EB%A6%AC%EC%8A%A4%ED%8A%B8,%20React%20FrontEnd-10533154.docx?d=w6c72945086d14ef1bf81039f3fcf4d52&csf=1&web=1&e=s8YZz8

--- wk04/tstodo/src/index.js
+++ wk04/tstodo/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 function App(){
   const [todos, setTodos] = React.useState([
@@ -15,17 +15,35 @@ function App(){
       text: 'build todo app',
       isCompleted: false,
     }        
-  ])
+  ]);//const
+
+  const [value, setValue] = React.useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos, {text:value, isCompleted:false}];
+    setTodos(newTodos);
+    setValue('');
+  }//const handleSubmit
 
   return(
     <>
-      {todos.map((todo, i) => (
-        <div className="todo" key={i}>{todo.text}</div>
-      ))}
-    </>)
-}
+      {todos.map((todo, i) => 
+        <div className="todo" key={i}>{todo.text}</div>)}
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
+      <form onSubmit={handleSubmit}>
+        <input
+            type="text" 
+            className="input" 
+            value = {value}
+            placeholder="Add Todo..."
+            onChange={e => setValue(e.target.value)}>
+        </input>
+      </form>
+      
+    </>
+  );//return
+};//App
+
+ReactDOM.render(<App/>,document.getElementById('root'));
