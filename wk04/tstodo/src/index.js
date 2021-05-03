8c59b8a953fb3ddfcf3517b9617169de01e3db9e
Insoo Kim <insoo@timmanage.com>
2021-05-03 15:16:10 +0900

    Remove by click and Add To-Do List on TIM Server
    implemented on "tsdodo" folder using create-react-app toolchain. Reference document: MIT xPRO-FEDR-4주차, 리액트 리스트, React FrontEnd-10533154  https://timmanagecom.sharepoint.com/:w:/r/sites/Merry/Shared%20Documents/MIT%20xPRO-FEDR-4%EC%A3%BC%EC%B0%A8,%20%EB%A6%AC%EC%95%A1%ED%8A%B8%20%EB%A6%AC%EC%8A%A4%ED%8A%B8,%20React%20FrontEnd-10533154.docx?d=w6c72945086d14ef1bf81039f3fcf4d52&csf=1&web=1&e=s8YZz8

--- wk04/tstodo/src/index.js
+++ wk04/tstodo/src/index.js
@@ -27,10 +27,17 @@ function App(){
     setValue('');
   }//const handleSubmit
 
+  const removeTodo = e => {
+    var index = Number(e.target.id);
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }//const removeTodo
+
   return(
     <>
       {todos.map((todo, i) => 
-        <div className="todo" key={i}>{todo.text}</div>)}
+        <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>)}
 
       <form onSubmit={handleSubmit}>
         <input
