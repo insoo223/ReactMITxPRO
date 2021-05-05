f8c4afd836969f4b39368b780612da60d422d9d2
Insoo Kim <insoo@timmanage.com>
2021-05-05 11:41:41 +0900

    array predefinedToDO for cleaner ode

--- wk04/tstodo/src/index.js
+++ wk04/tstodo/src/index.js
@@ -4,6 +4,22 @@ import {TodoForm} from './form'
 import {Todo} from './todo'
 
 function App(){
+  const predefinedToDO = [
+    {
+      text: 'Learn react(리액트 학습)',
+      isCompleted: false,
+    },
+    {
+      text: 'Meet friend for lunch(친구와 점심)',
+      isCompleted: false,
+    },
+    {
+      text: 'Build todo app(할일 앱 작성)',
+      isCompleted: false,
+    }        
+  ];
+  const [todos, setTodos] = React.useState(predefinedToDO);
+  /* 
   const [todos, setTodos] = React.useState([
     {
       text: 'Learn react(리액트 학습)',
@@ -18,7 +34,7 @@ function App(){
       isCompleted: false,
     }        
   ]);//const [todos, setTodos]
-
+  */
   const addTodo = text => {
     const newTodos = [...todos, {text:text, isCompleted:false}];
     setTodos(newTodos);
