fb3cda2a51e54b7584097cd450927fc3c54901b6
Insoo Kim <insoo@timmanage.com>
2021-05-05 16:48:08 +0900

    List Basics without using toolchain
    implemented on "salist" folder step-by-step setting dev environment. Ch 19.7  of Reference document: MIT xPRO-FEDR-4주차, 리액트 리스트, React FrontEnd-10533154  https://timmanagecom.sharepoint.com/:w:/r/sites/Merry/Shared%20Documents/MIT%20xPRO-FEDR-4%EC%A3%BC%EC%B0%A8,%20%EB%A6%AC%EC%95%A1%ED%8A%B8%20%EB%A6%AC%EC%8A%A4%ED%8A%B8,%20React%20FrontEnd-10533154.docx?d=w6c72945086d14ef1bf81039f3fcf4d52&csf=1&web=1&e=s8YZz8

--- wk04/list/src/bkup/todo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Todo({todo,index,remove}){
-  function handle(){
-    console.log('Ping:',index);
-    remove(index);
-  }
-  return <div className="todo" onClick={handle}>{todo.text} (-)</div>
-}
