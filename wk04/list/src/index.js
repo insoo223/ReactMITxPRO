69e7cda0308de5a5d7b095957e49e75fe4812951
Insoo Kim <insoo@timmanage.com>
2021-05-05 12:35:11 +0900

    List Basics
    implemented on "list" folder using create-react-app toolchain. Ch 19  of Reference document: MIT xPRO-FEDR-4주차, 리액트 리스트, React FrontEnd-10533154  https://timmanagecom.sharepoint.com/:w:/r/sites/Merry/Shared%20Documents/MIT%20xPRO-FEDR-4%EC%A3%BC%EC%B0%A8,%20%EB%A6%AC%EC%95%A1%ED%8A%B8%20%EB%A6%AC%EC%8A%A4%ED%8A%B8,%20React%20FrontEnd-10533154.docx?d=w6c72945086d14ef1bf81039f3fcf4d52&csf=1&web=1&e=s8YZz8

---
+++ wk04/list/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const numbers = [1, 2, 3, 4, 5];
+
+const updatedNums = numbers.map((number, index) => {
+  return <li key={index.toString()}>{number}</li>;
+});
+
+ReactDOM.render(<ul>{updatedNums}</ul>, document.getElementById("root"));
