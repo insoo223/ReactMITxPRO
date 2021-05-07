87684ede4f287a4e40f8fc28116de13a091f24d6
Insoo Kim <insoo@hotmail.com>
2021-05-07 14:57:32 +0900

    List w/button wo/onClick at InsooDesk
    Tested on InsooDesk C:\LocalData\GitHub\ReactMITxPRO\Wk04\listinsoodesk (Ch 19.8 of MIT xPRO-FEDR-4주차, 리액트 리스트, React FrontEnd-10533154)

--- wk04/listinsoodesk/src/index.js
+++ wk04/listinsoodesk/src/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as ReactBootstrap from 'react-bootstrap'
 
-const numbers = [1, 2, 3, 4, 5];
-
-const updatedNums = numbers.map((number, index) => {
-  return <li key={index.toString()}>{number*2}</li>;
-});
-
-ReactDOM.render(<ul>{updatedNums}</ul>, document.getElementById("root"));
-//ReactDOM.render(<ol>{updatedNums}</ol>, document.getElementById("root"));
+// use of "props" to set data
+function NavBar(props) {
+  const list = props.menuitems;
+  const { Button } = ReactBootstrap;
+  
+  console.log('Insoo\'s list rendering ');
+  // each item should have an unique key
+  const updatedList = list.map((item, index) => {
+    //log msg will be called 5 times at run-time.
+    //This is not called by onClick event but only called onece per web page loading
+    console.log('hi, insoo');
+    return(
+    <>
+      <Button key={index}>{item}</Button>
+    </>)
+  });
+  // note that React needs to have a single Parent
+  return <ul>{updatedList}</ul>;
+}
+const menuSrc = [1, 2, 3, 4, 5];
+ReactDOM.render(
+  <NavBar menuitems={menuSrc}/>,
+  document.getElementById("root")
+);
