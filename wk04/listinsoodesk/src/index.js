1192027a743d139a8f5a7bf1281ad8570f5cbaf0
Insoo Kim <insoo@hotmail.com>
2021-05-07 14:12:36 +0900

    plain List(*2) at InsooDesk
    Tested on InsooDesk C:\LocalData\GitHub\ReactMITxPRO\Wk04\listinsoodesk (Ch 19 of MIT xPRO-FEDR-4주차, 리액트 리스트, React FrontEnd-10533154)

--- wk04/listinsoodesk/src/index.js
+++ wk04/listinsoodesk/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const numbers = [1, 2, 3, 4, 5];
 
 const updatedNums = numbers.map((number, index) => {
-  return <li key={index.toString()}>{number}</li>;
+  return <li key={index.toString()}>{number*2}</li>;
 });
 
 ReactDOM.render(<ul>{updatedNums}</ul>, document.getElementById("root"));
