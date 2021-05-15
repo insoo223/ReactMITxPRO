2dfbc65503710072a4b5d022b0230bc64407a978
Insoo Kim <insoo@timmanage.com>
2021-05-15 12:38:39 +0900

    React Docs-Introducing JSX:Embed ft. return
    Ref: https://reactjs.org/docs/rendering-elements.html

--- reactdocs/src/index.js
+++ reactdocs/src/index.js
@@ -1,36 +1,29 @@
 /** React Docs-Introducing JSX:Embed ft. return
- * Ref: https://reactjs.org/docs/introducing-jsx.html
+ * Ref: https://reactjs.org/docs/rendering-elements.html
  */
 //리액트 문서오브젝트모델(react-dom)이 렌더링 엔진 모듈
 import ReactDOM from 'react-dom'; 
 
-function formatName(person) {
-  return person.firstName + ' ' + person.lastName;
-}
-
-const user = {
-  firstName: 'Insoo',
-  lastName: 'Kim'
-}
 
 //JSX에 함수 결과값을 받게, JSX에서 중괄호 안에 어떤 JS표현이라도 가능.
 //가독성 위해 여러 줄에 걸쳐 표현하고, JSX를 괄호로 감싸는 게 좋다.
 const element = (
 <h1>
-  Hello,{formatName(user)}
+  Hello,World!
 </h1>
 );
 
-/** document.getElementById('root')는 웹 DOM의 리액트 확장판
+/** ReactDOM()은 React element의 변화된 부분만 인식해서 새로운 React element를 생성, 갱신
  * 요약: 
- * 1.웹 문서 오브젝트 모델(웹돔)이란 구조 및 내용으로 이뤄진 웹문서 오브젝트 데이터 모델링(표현)을 의미.
- * 
- * 2.HTML 및 XML 프로그래밍 인터페이스
- * 3.웹 문서 구조, 스타일 및 내용을 프로그래밍으로 변경하기 위한 웹 페이지 표상.
- * 4.웹 문서를 노드와 오브젝트로 표현해서 프로그래밍 언어가 웹 페이지를 연결할 수 있음.
- * 5.결국, API = DOM + JavaScript
+ * 1.React element는 root DOM node (html화일의  <div id="root"></div> 부분)에서 렌더링됨
+ * 2.ReactDOM.render()함수에 React element와  root  DOM node를 전달하면 끝.
+ * 3.React element는 렌더된 후 바뀌지 않음(immutable). 따라서, 새로운 React element를 생성해야하는데, React DOM은 React element에서 변화된 부분만 골라내어 새로운 React element를 생성 및 렌더링 한다.
+ * 4.사람은 시간변화에 대해 생각하지 않고, “한 시점에서 웹 페이지가 어떻게 보여야 함”만 고려하면 된다. 변화관리는 React DOM이 한다. 물론, 변화기제 즉 시간 tick발생기 등은 코더가 제공
+ * 5.다시말해, React element의 어떤 부분이 어떻게 바꾸었는지 판단하고 가장 최소 메모리와 컴퓨팅 비용을 들여서 렌러링 로직을 작성해 실행하는 것은 React DOM의 몫이다.
+ 
  * 참조
- * 1.웹돔: https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction
+ * 1.React: Rethinking best practice by Pete Hunt (JSConf EU 2013)
+ * https://www.youtube.com/watch?v=x7cQ3mrcKaY 
  **/
 ReactDOM.render(
   element,
