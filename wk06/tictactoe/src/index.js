b4ed5841b4b505f9c5117630803412fd55a92b40
Insoo Kim <insoo@timmanage.com>
2021-05-21 14:15:21 +0900

    Add Squares As Child Components
    Ref: https://student.emeritus.org/courses/2663/pages/video-6-3-6-4-15-35-generate-players-on-the-tic-tac-toe-board-and-control-component-with-onclick-events?module_item_id=582137

--- wk06/tictactoe/src/index.js
+++ wk06/tictactoe/src/index.js
@@ -1,23 +1,26 @@
-/*** 3.2	Control Component With OnClick Events
+/*** 4.1	Add Squares As Child Components
  * Ref: https://student.emeritus.org/courses/2663/pages/video-6-3-6-4-15-35-generate-players-on-the-tic-tac-toe-board-and-control-component-with-onclick-events?module_item_id=582137
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Square = () => {
+  return <button type="button"></button>; //return
+}//Square
+
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
   let status = `Player ${player}`;
+  function renderSquare (i) {
+    return <Square></Square>;
+  }
   return (
-    <div className="game-board" 
-      onClick={e =>
-        {
-          setPlayer((player+1)%2);
-          status = `Player ${player}`;
-          e.target.style.background="red";
-          e.target.style.width=200;
-        }
-      }
-    >
+    <div className="game-board" >
+      <div className="grid-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
       <div id="info">
         <h1>{status}</h1>
       </div>
