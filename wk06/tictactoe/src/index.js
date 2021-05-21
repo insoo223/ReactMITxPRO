057998fdf63f51d695ffd3c8a9c937b7617ef659
Insoo Kim <insoo@timmanage.com>
2021-05-21 14:32:12 +0900

    4.2 Pass ID As State From Parent To Child: only id wo/props
    Ref: https://student.emeritus.org/courses/2663/pages/video-6-5-6-6-7-35-add-squares-as-child-components-and-pass-id-as-state-from-parent-to-child?module_item_id=582139

--- wk06/tictactoe/src/index.js
+++ wk06/tictactoe/src/index.js
@@ -1,11 +1,15 @@
-/*** 4.2	Pass ID As State From Parent To Child: props.id
+/*** 4.2	Pass ID As State From Parent To Child: only id wo/props
  * Ref: https://student.emeritus.org/courses/2663/pages/video-6-5-6-6-7-35-add-squares-as-child-components-and-pass-id-as-state-from-parent-to-child?module_item_id=582139
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Square = (props) => {
-  return <button type="button">{props.id}</button>; //return
+const Square = ({id}) => {
+  return (
+    <button type="button">
+      <h1>{id}</h1>
+    </button>
+  ); //return
 }//Square
 
 const Board = () => {
