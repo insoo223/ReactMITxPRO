5b4a7377da3334c396621b27b92e0cd2b060b530
Insoo Kim <insoo@timmanage.com>
2021-05-22 10:59:47 +0900

    9 Component Lifecycle: Show/Hide button square row
    Ref: https://student.emeritus.org/courses/2663/pages/video-6-9-4-09-component-lifecycle?module_item_id=582145

--- wk06/tictactoe/src/index.js
+++ wk06/tictactoe/src/index.js
@@ -1,50 +1,58 @@
-/*** 7 Pass State From Child To Parent
- * Ref: https://student.emeritus.org/courses/2663/pages/video-6-8-4-39-pass-state-from-child-to-parent?module_item_id=582142
+/*** 9 Component Lifecycle: Show/Hide button square row
+ * Ref: https://student.emeritus.org/courses/2663/pages/video-6-9-4-09-component-lifecycle?module_item_id=582145
  */
-import React from 'react'
-import ReactDOM from 'react-dom'
+ import React from 'react'
+ import ReactDOM from 'react-dom'
+ 
+ 
+ //Child
+ const Square = ({id, player}) => {
 
+   const [color, setColor] = React.useState('green');
+   const palet = ["red", "green", "blue"];
 
-//Child
-const Square = ({id, player}) => {
-  const [color, setColor] = React.useState('green');
-  const palet = ["red", "green", "blue"];
-  const getRandomColor = () => palet[Math.floor(Math.random()*3)];
+   const getRandomColor = () => palet[Math.floor(Math.random()*3)];
+ 
+   return (
+     <button type="button" 
+       onClick={ e => 
+         {
+           setColor(getRandomColor());
+           e.target.style.background = color;
+           alert(`I'm Square ${id}`);
+         }
+       }
+     >
+         <h1>{id}:{player}</h1>
+     </button>
+   ); //return
+ }//Square
+ 
+ //Parent
+ const Board = () => {
+   const [player, setPlayer] = React.useState(1);
+   let status = `Player ${player}`;
+   const [mounted, setMounted] = React.useState(true);
 
-  return (
-    <button type="button" 
-      onClick={ e => 
-        {
-          setColor(getRandomColor());
-          e.target.style.background = color;
-          alert(`I'm Square ${id}`);
-        }
-      }
-    >
-        <h1>{id}:{player}</h1>
-    </button>
-  ); //return
-}//Square
-
-//Parent
-const Board = () => {
-  const [player, setPlayer] = React.useState(1);
-  let status = `Player ${player}`;
-  function renderSquare (i) {
-    return <Square id={i} player={player}></Square>;
-  }
-  return (
-    <div className="game-board" >
-      <div className="grid-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div id="info">
-        <h1>{status}</h1>
-      </div>
-    </div>
-  )//return
-}//Board
-
-ReactDOM.render(<Board />, document.getElementById('root'));
+   const toggle = () => setMounted(!mounted);
+   function renderSquare (i) {
+     return <Square id={i} player={player}></Square>;
+   }
+   return (
+     <div className="game-board" >
+       <div className="grid-row">
+         {mounted && renderSquare(0)}
+         {mounted && renderSquare(1)}
+         {mounted && renderSquare(2)}
+       </div>
+       <div>
+         <button onClick={toggle}>Show/Hide Row</button>
+       </div>
+       <div id="info">
+         <h1>{status}</h1>
+       </div>
+     </div>
+   )//return
+ }//Board
+ 
+ ReactDOM.render(<Board />, document.getElementById('root'));
