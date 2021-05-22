bbfd4792ecf5e70d6df364de8824b6043e4a0d64
Insoo Kim <insoo@timmanage.com>
2021-05-22 11:31:36 +0900

    10 Mounting And Unmounting Square Components: Template code, Show/Hide Individual Square Button
    Ref: https://student.emeritus.org/courses/2663/assignments/107298?module_item_id=582146

--- wk06/tictactoe/src/index.js
+++ wk06/tictactoe/src/index.js
@@ -1,58 +1,79 @@
-/*** 9 Component Lifecycle: Show/Hide button square row
- * Ref: https://student.emeritus.org/courses/2663/pages/video-6-9-4-09-component-lifecycle?module_item_id=582145
+/*** 10 Mounting And Unmounting Square Components: Template code, Show/Hide Individual Square Button
+ * Ref: https://student.emeritus.org/courses/2663/assignments/107298?module_item_id=582146
  */
  import React from 'react'
  import ReactDOM from 'react-dom'
  
- 
- //Child
- const Square = ({id, player}) => {
+ const Square = ({ takeTurn, id }) => {
+  const mark = ['O', 'X', '+'];
+  // id is the square's number
+  // filled tells you if square has been filled
+  // tik tells you symbol in square (same as player)
+  // call takeTurn to tell Parent the square is  filled
+  const [filled, setFilled] = React.useState(false);
+  const [tik, setTik] = React.useState(2);
 
-   const [color, setColor] = React.useState('green');
-   const palet = ["red", "green", "blue"];
+  // Step 1: Move the mounted state & toggle from the board component to here
 
-   const getRandomColor = () => palet[Math.floor(Math.random()*3)];
- 
-   return (
-     <button type="button" 
-       onClick={ e => 
-         {
-           setColor(getRandomColor());
-           e.target.style.background = color;
-           alert(`I'm Square ${id}`);
-         }
-       }
-     >
-         <h1>{id}:{player}</h1>
-     </button>
-   ); //return
- }//Square
- 
- //Parent
- const Board = () => {
-   const [player, setPlayer] = React.useState(1);
-   let status = `Player ${player}`;
-   const [mounted, setMounted] = React.useState(true);
+  // Step 2: Check to see if the mounted state is false. If it is we want to return null instead of returning the button
 
-   const toggle = () => setMounted(!mounted);
-   function renderSquare (i) {
-     return <Square id={i} player={player}></Square>;
-   }
-   return (
-     <div className="game-board" >
-       <div className="grid-row">
-         {mounted && renderSquare(0)}
-         {mounted && renderSquare(1)}
-         {mounted && renderSquare(2)}
-       </div>
-       <div>
-         <button onClick={toggle}>Show/Hide Row</button>
-       </div>
-       <div id="info">
-         <h1>{status}</h1>
-       </div>
-     </div>
-   )//return
- }//Board
- 
- ReactDOM.render(<Board />, document.getElementById('root'));
+  return (
+    // Step 3: Trigger toggle() when button is clicked
+    <button
+      // DO NOT DELETE THIS id
+      id={`square-button-${id}`}
+      onClick={() => {
+        setTik(takeTurn(id));
+        setFilled(true);
+      }}
+    >
+      <h1>{mark[tik]}</h1>
+    </button>
+  );
+};
+
+const Board = () => {
+  // 1st player is X ie 1
+  // State keeps track of next player and gameState
+  const [player, setPlayer] = React.useState(1);
+  const [mounted, setMounted] = React.useState(true);
+  const [gameState, setGameState] = React.useState([]);
+
+  const toggle = () => setMounted(!mounted);
+
+  const takeTurn = (id) => {
+    setGameState([...gameState, { id: id, player: player }]);
+    setPlayer((player + 1) % 2); // get next player
+    return player;
+  };
+  function renderSquare(i) {
+    // use properties to pass callback function takeTurn to Child
+    return <Square takeTurn={takeTurn} id={i}></Square>;
+  }
+
+  return (
+    <div className="game-board">
+      <div className="grid-row">
+        {mounted && renderSquare(0)}
+        {mounted && renderSquare(1)}
+        {mounted && renderSquare(2)}
+      </div>
+      <div id="info">
+        <button onClick={toggle}>Show/Hide Row</button>
+        <h1>{status}</h1>
+      </div>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div className="game">
+      <Board></Board>
+    </div>
+  );
+};
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
