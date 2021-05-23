df84bc4344be847c751b1ff58b9d274468ae2f30
Insoo Kim <insoo@timmanage.com>
2021-05-23 10:01:30 +0900

    15 Zombie-battle:Communication bet. paranet(GameMgr) & child(Zombie) components
    Ref: How to Handle Communication Between Parent and Child Components in ReactJS https://www.pluralsight.com/guides/how-to-handle-communication-between-parent-and-child-components-in-reactjs

--- wk06/zombie-battle/src/App.js
+++ wk06/zombie-battle/src/App.js
@@ -1,4 +1,4 @@
-/*** 15 Zombie-battle: Set Template for GameMgr, Zombie components
+/*** 15 Zombie-battle:Communication bet. paranet(GameMgr) & child(Zombie) components
  * Ref: How to Handle Communication Between Parent and Child Components in ReactJS https://www.pluralsight.com/guides/how-to-handle-communication-between-parent-and-child-components-in-reactjs
  */
 import React, { Component } from 'react';
--- wk06/zombie-battle/src/components/GameMgr/index.jsx
+++ wk06/zombie-battle/src/components/GameMgr/index.jsx
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
+
 import Zombie from '../Zombie';
 
 const Humbug = {
     name: 'Humbug',
     level: 5,
-    hp: 20
+    hp: 20,
+    type: 'cpu',
+    moves: {
+        move_1: {
+            name: "Tackle",
+            power: 4
+        },
+        move_2: {
+            name: "Growl",
+            power: 0
+        }
+    }
 }
 
 class GameMgr extends Component {
     constructor(props) {
         super(props);
+        this.state = {}
+    }
+
+    getAttack = (zombie, move) => {
+        let newState = this.state;
+        newState = {
+            attack: {
+                zombie: zombie,
+                move: move.name
+            }
+        }
+        this.setState(newState);
+    }
+
+    showMessage = () => {
+        if(this.state.attack) {
+            return(
+                <div>
+                    <p>{`${this.state.attack.zombie} uses ${this.state.attack.move}!`}</p>
+                </div>
+            )
+        }
+        return false;
     }
 
     render() {
         return(
             <div>
-                <Zombie name={"Geek"} level={5} hp={21}></Zombie>
-                <Zombie {...Humbug}></Zombie>
+                <Zombie 
+                    type='player' 
+                    name={"Geek"} 
+                    level={6} 
+                    hp={11} 
+                    moves={{move_1: {name: "Scratch", power: 5},
+                            move_2: {name: "Leer", power: 0}}}
+                    sendAttack={this.getAttack.bind(this)}>
+                </Zombie>
+                <Zombie {...Humbug} sendAttack={this.getAttack.bind(this)}></Zombie>
+                { this.showMessage() }
             </div>
         )
     }
