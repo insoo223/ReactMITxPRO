c3f68e5a359ef6aa38a9b841edf6425a2fe3796f
Insoo Kim <insoo@timmanage.com>
2021-05-24 11:50:24 +0900

    17.1 Passing Player From Parent To Child
    Ref: https://student.emeritus.org/courses/2663/pages/video-6-13-6-14-14-51-passing-player-from-parent-to-child-and-tracking-total-game-state?module_item_id=582153

--- wk06/tictactoe/src/index.js
+++ wk06/tictactoe/src/index.js
@@ -1,14 +1,15 @@
-/*** 16 UseState Delay: asynchronous update of useState
- * Ref: https://student.emeritus.org/courses/2663/pages/video-6-12-3-47-usestate-delay?module_item_id=582152
+/*** 17.1 Passing Player From Parent To Child
+ * Ref: https://student.emeritus.org/courses/2663/pages/video-6-13-6-14-14-51-passing-player-from-parent-to-child-and-tracking-total-game-state?module_item_id=582153
  */
  import React from 'react'
  import ReactDOM from 'react-dom'
  
 
  //Child
- const Square = ({id, player}) => {
-
+ const Square = ({id, newState}) => {
    const [color, setColor] = React.useState('green');
+   const [status, setStatus] = React.useState(null);
+   const xo = ["O", "X"];
    const palet = ["red", "green", "blue"];
    const getRandomColor = () => palet[Math.floor(Math.random()*3)];
    React.useEffect( () => 
@@ -24,12 +25,13 @@
          {
            let col = getRandomColor();
            setColor(col);
-           e.target.style.background = color;
-           //alert(`I'm Square ${id}`);
+           let nextPlayer = newState({id:id, color:col});
+           setStatus(nextPlayer);
+           e.target.style.background = col;
          }
        }
      >
-         <h1>{id}</h1>
+         <h1>{xo[status]}</h1>
      </button>
    ); //return
  }//Square
@@ -37,12 +39,22 @@
  //Parent
  const Board = () => {
    const [player, setPlayer] = React.useState(1);
+   const [state, setState] = React.useState([]);
    let status = `Player ${player}`;
    const [mounted, setMounted] = React.useState(true);
-
    const toggle = () => setMounted(!mounted);
+   const newState = ob =>
+   {
+     let nextPlayer = (player+1)%2;
+     setPlayer(nextPlayer);
+     setState([...state, ob]);
+     console.log(`adding state ${JSON.stringify(state)}`);
+     status = `Player ${nextPlayer}`;
+     return nextPlayer;
+   }//newState
+
    function renderSquare (i) {
-     return <Square id={i} player={player}></Square>;
+     return <Square id={i} player={player} newState={newState}></Square>;
    }
    return (
      <div className="game-board" >
