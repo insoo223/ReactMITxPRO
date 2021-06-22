2475ec38f0a99b1431c7f8ef7f22132e70c59ee4
Insoo Kim <insoo@timmanage.com>
2021-06-22 12:00:20 +0900

    cartDB strapi fetching with GraphQL
    MIT xPRO-FEDR-10주차, 테스팅, React FrontEnd-10533160

---
+++ wk10/cartDB/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bd1d5412-ccd9-448a-88a7-a40329658983'
+};
---
+++ wk10/restaurants/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '77e1c6fc-973e-46bf-bfa7-9b13f183d0bb'
+};
