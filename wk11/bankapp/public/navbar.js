aaa77f0b23d57f1493e5cc1e587634b048748ef8
Insoo Kim <insoo@timmanage.com>
2021-06-28 17:18:37 +0900

    Navigation Bar Background Color
    Ch12, MIT xPRO-FEDR-GitHub Portfolio Project-React FrontEnd-10533161

--- wk11/bankapp/public/navbar.js
+++ wk11/bankapp/public/navbar.js
@@ -1,7 +1,7 @@
 function NavBar(){
   return(
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="#" data-placement="top" title="To return to home page, click this link please.">Insoo Bank</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
