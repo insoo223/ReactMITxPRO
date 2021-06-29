a64a81fe9dae6bf42ccc671203a2db4f066449e8
Insoo Kim <insoo@timmanage.com>
2021-06-29 10:34:13 +0900

    useState parenthesis correction
    mistake by using { } instead of [ ]

--- wk11/bankapp/public/deposit.js
+++ wk11/bankapp/public/deposit.js
@@ -1,8 +1,8 @@
 function Deposit(){
-  const {deposit, setDeposit} = React.useState('');
-  const {show, setShow} = React.useState(true);
-  const {status, setStatus} = React.useState('');
-
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState('');
+  const [deposit, setDeposit] = React.useState('');
+  
   return (
     <Card
       bgcolor="success"
@@ -10,6 +10,9 @@ function Deposit(){
       status={status}
       body={show ? (
         <>
+          Deposit Amount<br/>
+          <input type="input" className="form-control" id="amount" placeholder="Amount" value={deposit} data-toggle="tooltip" data-placement="top" title="Enter your deposit amount, please." onChange={e => {setDeposit(e.currentTarget.value)}} 
+          /><br/>
         </>
       ):(
         <>
