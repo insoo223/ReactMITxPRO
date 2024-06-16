cb940ec673a5ca13c83344b7f4c8f7249ebaa790
Insoo Kim <insoo@hotmail.com>
2024-06-16 17:55:57 +0900

    react bootstrap installed

--- wk08/vitebank/src/main.jsx
+++ wk08/vitebank/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import App from "./App";
+import Home from './home';
+import BasicExample from './navbar.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// import "./index.css";
+// import glbStr from "./String";
+
+const router = createBrowserRouter([
+  //root /
+  { 
+    // path: `/` , element: <Home />,
+    path: `/` , element: <BasicExample />,
+    // path: `/` , element: <App />,
+    // path: `${glbStr.routeRoot}` , element: <App />,
+    children: [{ path: `/`, element: <Home />,},],
+  },
+
+    
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+    {/* <BasicExample /> */}
+  </React.StrictMode>
+);
