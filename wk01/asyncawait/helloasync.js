93f9aa7a08df7135fbf448ad05d52d2380588729
insoo223 <insoo@hotmail.com>
2025-03-17 11:25:45 +0900

    wait 2sec and print out results simultaneously

--- wk01/asyncawait/helloasync.js
+++ wk01/asyncawait/helloasync.js
@@ -2,12 +2,17 @@
 1. File: helloasync.js
 2. 2. How to Run: $node helloasync.js
 3. Exprected Result: 
+
+insoo@vintrix ~/github/ReactMITxPRO/wk01/asyncawait
+ % node helloasync.js
+calling... 
 calling...
 what a wonderful world...
 still wait...
 what a wonderful world...
 still wait...
-resolved <-- (this will be printed out after two seconds)
+resolved1 <-- (these two results will be simultaneously printed out after two seconds)
+resolved2
 
 4. PN 1033151 (p44) of the printed binding of "MIT xPRO Front-End Dev. w/React"
 
@@ -29,12 +34,16 @@ function resolveAfter2sec2() {
 	});
 }
 
-async function asyncCall() {
+async function asyncCall1() {
 	console.log('calling...');
-	const result1 = await resolveAfter2sec1();
-	const result2 = await resolveAfter2sec2();
-	console.log(result1);
-	console.log(result2);
+	const result = await resolveAfter2sec1();
+	console.log(result);
+}
+
+async function asyncCall2() {
+	console.log('calling...');
+	const result = await resolveAfter2sec2();
+	console.log(result);
 }
 
 function lulurara()
@@ -45,5 +54,6 @@ function lulurara()
 	console.log('still wait...');
 }
 	
-asyncCall();
+asyncCall1();
+asyncCall2();
 lulurara();
